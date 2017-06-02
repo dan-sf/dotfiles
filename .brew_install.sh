7da488bc9e4cdebe545839a38c2a359c7fc1f401
dsfcode <fowler.dn@gmail.com>
2017-06-02 09:29:36 -0700

    add common brew install commands

---
+++ .brew_install.sh
@@ -0,0 +1,17 @@
+# Get brew if not installed
+[[ `which brew &> /dev/null; echo $?` != 0 ]] && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install packages
+brew install dos2unix
+brew install findutils --with-default-names
+brew install gawk
+brew install maven
+brew install mysql
+brew install perl
+brew install python
+brew install python3
+brew install rename
+brew install tmux
+brew install tree
+brew install wget
+
